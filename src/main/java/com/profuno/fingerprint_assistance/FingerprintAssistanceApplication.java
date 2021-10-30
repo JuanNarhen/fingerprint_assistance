78c8e2178b848313ea839d5f506a11c1923a363c
JuanNarhen <cami.naranjo123@gmail.com>
2021-10-30 17:51:20 -0500

    SCHD-Initial Commit

---
+++ src/main/java/com/profuno/fingerprint_assistance/FingerprintAssistanceApplication.java
@@ -0,0 +1,13 @@
+package com.profuno.fingerprint_assistance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FingerprintAssistanceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FingerprintAssistanceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/profuno/fingerprint_assistance/FingerprintAssistanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.profuno.fingerprint_assistance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FingerprintAssistanceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
